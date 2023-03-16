9b950dfc1bd53d2c92ee9f58cb4291164d118fc7
Taicheng Wu <wutaicheng1999@gmail.com>
2023-03-16 17:48:38 -0400

    fix zooming when radius is selected

--- availability_viewer/src/MapComponent.js
+++ availability_viewer/src/MapComponent.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 
 function MapComponent({ location, radius }) {
   const [map, setMap] = useState(null);
-  const [circle, setCircle] = useState(null);
   const [center, setCenter] = useState(null);
 
   useEffect(() => {
@@ -18,35 +17,13 @@ function MapComponent({ location, radius }) {
         // Create the map centered on the selected location
         const map = new googleMaps.Map(document.getElementById('map'), {
           center: latLng,
-          zoom: 12,
+          zoom: Math.round(14 - Math.log(radius) / Math.LN2),
         });
         setMap(map);
-
-        // Create a circle overlay representing the selected radius
-        const circle = new googleMaps.Circle({
-          strokeColor: '#FF0000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: '#FF0000',
-          fillOpacity: 0.35,
-          map: map,
-          center: latLng,
-          radius: radius * 1609.34, // Convert miles to meters
-        });
-        setCircle(circle);
-
-        // Set the center state variable for later use
         setCenter(latLng);
       }
     });
   }, [location, radius]);
-
-  useEffect(() => {
-    // Update the circle radius when the radius prop changes
-    if (circle) {
-      circle.setRadius(radius * 1609.34); // Convert miles to meters
-    }
-  }, [radius, circle]);
 
   return (
     <div id="map" style={{ height: '400px', width: '100%' }}>
