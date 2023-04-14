4d5e17f7c4649ea08125dacefb37ae3d1430c1ef
Billy Nowosielski <billyn@bu.edu>
2023-04-14 02:33:36 -0400

    converted to typescript

---
+++ availability_viewer/src/MapComponent.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+
+interface MapProps {
+  location: string;
+  radius: number;
+  onLoad: (map: google.maps.Map) => void;
+  onMarkerLoad: (marker: google.maps.Marker) => void;
+}
+
+function MapComponent({ location, radius, onLoad, onMarkerLoad }: MapProps) {
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [center, setCenter] = useState<google.maps.LatLng | null>(null);
+  const [marker, setMarker] = useState<google.maps.Marker | null>(null);
+
+  useEffect(() => {
+    // Create a new Google Maps API instance
+    const googleMaps = window.google.maps;
+
+    // Geocode the selected location to get its coordinates
+    const geocoder = new googleMaps.Geocoder();
+    geocoder.geocode({ address: location }, (results, status) => {
+      if (status === googleMaps.GeocoderStatus.OK && results) {
+        const latLng = results[0].geometry.location;
+
+        // Create the map centered on the selected location
+        const map = new googleMaps.Map(document.getElementById('map')!, {
+          center: latLng,
+          zoom: Math.round(14 - Math.log(radius) / Math.LN2),
+        });
+        setMap(map);
+        setCenter(latLng);
+
+        // Call the onLoad function with the map instance
+        onLoad(map);
+
+        // Create a marker at the selected location
+        const marker = new googleMaps.Marker({
+          position: latLng,
+          map,
+          visible: false,
+          title: location,
+        });
+        setMarker(marker);
+
+        // Call the onMarkerLoad function with the marker instance
+        onMarkerLoad(marker);
+      }
+    });
+  }, [location, radius]);
+
+  return (
+    <div id="map" style={{ height: '400px', width: '100%' }}>
+      {/* The Google Maps API will render the map here */}
+    </div>
+  );
+}
+
+export default MapComponent;
+
---
+++ availability_viewer/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import reportWebVitals from './reportWebVitals.tsx';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  reportWebVitals();
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals((metric) => {
+  console.log(metric);
+});
