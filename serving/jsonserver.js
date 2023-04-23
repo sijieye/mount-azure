506a25c6e0518e1dde99c6ae4963c7224e2642e1
sijieye <sijieye@bu.edu>
2023-04-23 01:23:20 -0400

    Add json server

--- serving/jsonserver.js
+++ serving/jsonserver.js
@@ -3,8 +3,10 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+const port = process.env.PORT || 8080;
+
 server.use(middlewares)
 server.use(router)
-server.listen(process.env.PORT, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
