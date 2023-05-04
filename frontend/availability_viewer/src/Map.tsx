6ab5002d7d7b0bcc471bb50cc5de3d8497234b09
sijieye <sijieye@bu.edu>
2023-05-04 19:43:36 -0400

    Update api 2

--- frontend/availability_viewer/src/Map.tsx
+++ frontend/availability_viewer/src/Map.tsx
@@ -126,7 +126,7 @@ function Map() {
           <label style={{fontSize: '40px'}}>
             Enter Location:
             <GoogleAutocomplete
-              apiKey={process.env.REACT_APP_GOOGLE_API_KEY}
+              apiKey={AIzaSyATZWTQjFZVElmC_pXyTz9XNgSJftqhz5I}
               onPlaceSelected={handlePlaceSelect}
               types={['(regions)']}
               placeholder="Enter a location"
